e4334075012a0756df7c93ccc5e2ec3247bcd56d
Himanshu <himansh.notes@gmail.com>
2026-09-14 14:59:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    // code here
+    while(x>=0){
+        cout<< x <<" ";
+        x--;
+    }
+
+    return 0;
+}
