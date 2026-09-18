581fa0434888cf42c28538dcf7ea091c6aec714a
Himanshu <himansh.notes@gmail.com>
2026-09-18 18:05:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Inverted Triangle of Stars/inverted-triangle-of-stars.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // code here
+    for(int i= 0;i<n;i++){
+        for(int j =0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
